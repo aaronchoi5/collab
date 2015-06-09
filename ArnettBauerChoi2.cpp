32a6a9b60b54258aa9d1422fd5b455e551c8902a
ajarnett94 <ajernett94@gmail.com>
2015-06-09 03:19:24 -0400

    Algorithm 1 is buggy

--- ArnettBauerChoi2.cpp
+++ ArnettBauerChoi2.cpp
@@ -4,7 +4,8 @@ Course: Data Structures
 Instructor: Anca Ralescu
 TA: Suryadip Chakraborty
 Abstract:  The program times 3 different algorithms for checking different sized arrays for repeated elements.
-Preconditions:  none
+Preconditions:  User must be able to use the stimulus in his or her eye to view the screen and
+                observe the efficiency of each IsUnique function.
 Postconditions: returns a table with the times of the different algorithms on different sized arrays.
 */
 
@@ -21,9 +22,21 @@ using namespace std;
 void randomArray(int length, int A[]){      //assigns values to the inputted array.
     srand((unsigned)time(NULL));
     for(int k=0; k<length; k++){
-        A[k]=rand() % (length*2) +1;
-        cout<<"value given: "<<A[k]<<endl;
+        A[k]=rand() % (length*10) + 1;
     }
+    return;
+}
+
+void PrintArray(int length, int A[]){
+    if(length>20){
+        return;
+    }
+    std::cout << "Given: [";
+    for(int i = 0; i < length-1; i++){
+        std::cout << A[i] << ", ";
+    }
+    std::cout << A[length-1] << "]" << std::endl;
+
     return;
 }
 
@@ -63,88 +76,32 @@ void bubbleSort(int A[], int length){   //classic bubble sort
     return;
 }
 
-void vectorSelectSort(vector<int> A){//helper function for bucketsort
-    int tempArray[A.size()];
-    for(unsigned int k=0; k<A.size(); k++){   //make vector into array
-        tempArray[k]=A[k];
-    }
-    selectionSort(tempArray, A.size());
-    for(unsigned int k=0; k<A.size(); k++){    //put sorted array into vector
-        A[k]=tempArray[k];
-    }
-    return;
-}
+void QuickSort(int Array[], int first, int last){
+    int i = first, j = last;
+    int tmp;
+    int pivot = Array[first + (last-first)/2];
 
-void bucketSort(int Array[], int length){
-    int minni=Array[0];
-    int maxxi=Array[0];
-    for(int k=0; k<length; k++){
-        if(Array[k]<minni){
-            minni=Array[k];
+    // partition
+    while (i <= j) {
+        while (Array[i] < pivot)
+              i++;
+        while (Array[j] > pivot)
+              j--;
+        if (i <= j) {
+              tmp = Array[i];
+              Array[i] = Array[j];
+              Array[j] = tmp;
+              i++;
+              j--;
         }
-        if(Array[k]>maxxi){
-            maxxi=Array[k];
-        }
-    }
-    if(length<=20){
-        selectionSort(Array,length);
-        //cout<<"SELECTION SORT USED"<<endl;
-    }
-    else{
-        int fakerange=(maxxi-minni)-((maxxi-minni)%5);
-        vector<int> firstVector;  //buckets
-        vector<int> secondVector;
-        vector<int> thirdVector;
-        vector<int> fourthVector;
-        vector<int> fifthVector;
-        int firstMax=fakerange/5+minni;
-        int secondMax=(fakerange/5)*2+minni;
-        int thirdMax=(fakerange/5)*3+minni;
-        int fourthMax=(fakerange/5)*4+minni;
-        for(int k=0; k<length; k++){       //sorts the array into the buckets
-            if(Array[k]<=firstMax){
-                firstVector.push_back(Array[k]);
-            }
-            else if(Array[k]<=secondMax){
-                secondVector.push_back(Array[k]);
-            }
-            else if(Array[k]<=thirdMax){
-                thirdVector.push_back(Array[k]);
-            }
-            else if(Array[k]<=fourthMax){
-                fourthVector.push_back(Array[k]);
-            }
-            else if(Array[k]>fourthMax){
-                fifthVector.push_back(Array[k]);
-            }
-        }
-        vectorSelectSort(firstVector);  //sorts the vectors with selection sort
-        vectorSelectSort(secondVector);
-        vectorSelectSort(thirdVector);
-        vectorSelectSort(fourthVector);
-        vectorSelectSort(fifthVector);
+    };
 
-        int counter=0;
-        for(unsigned int k=counter; k<firstVector.size(); k++){       //combines the vectors into the array
-            Array[k]=firstVector[k];
-        }
-        counter=firstVector.size();
-        for(unsigned int k=counter; k<counter+secondVector.size(); k++){
-            Array[k]=secondVector[k];
-        }
-        counter=counter+secondVector.size();
-        for(unsigned int k=counter; k<counter+thirdVector.size(); k++){
-            Array[k]=thirdVector[k];
-        }
-        counter=counter+thirdVector.size();
-        for(unsigned int k=counter; k<counter+fourthVector.size(); k++){
-            Array[k]=fourthVector[k];
-        }
-        counter=counter+fourthVector.size();
-        for(unsigned int k=counter; k<counter+fifthVector.size(); k++){
-            Array[k]=fifthVector[k];
-        }
-    }
+    // recursion
+    if (first < j)
+        QuickSort(Array, first, j);
+
+    if (i < last)
+        QuickSort(Array, i, last);
     return;
 }
 
@@ -158,6 +115,8 @@ bool isUnique1(int A[], int first, int last){
     if(!isUnique1(A, first+1, last)){
        return false;
     }
+//std::cout << "test" << std::endl;
+//std::cout << first << " " << A[first] << " " << last << " " << A[last] << std::endl;
     return (A[first]!=A[last]);
 }
 
@@ -180,10 +139,10 @@ bool isUnique3(int A[], int first, int last){
     if(first>=last){
         return true;
     }
-    
-    bucketSort(A, last);//assuming bucket sort is the fastest
-    for(int i=first; i<last; i++){
-        if(A[i]==A[i+1]){
+
+    QuickSort(A, first, last);//assuming quick sort is the fastest
+    for(int i = first; i < last; i++){
+        if(A[i] == A[i+1]){
             return false;
         }
     }
@@ -191,38 +150,45 @@ bool isUnique3(int A[], int first, int last){
 }
 
 int main(){
-    int length=20;
+    int length=50;
     int Array[length];
+std::cout << "Array has been initialized" << std::endl;
     randomArray(length,Array);
+std::cout << "After random generation" << std::endl;
+    PrintArray(length,Array);
     int bubbleArray[length];
     for(int k=0; k<length; k++){  //makes bubbleArray a copy of Array
         bubbleArray[k]=Array[k];
     }
     bubbleSort(bubbleArray, length);
-
+std::cout << "after bubble sort" << std::endl;
     int selectionArray[length];
     for(int k=0; k<length; k++){  //makes selectionArray a copy of Array
         selectionArray[k]=Array[k];
     }
     selectionSort(selectionArray, length);  // selection sort
-
-    int bucketArray[length];
-    for(int k=0; k<length; k++){  //makes bucketArray a copy of Array
-        bucketArray[k]=Array[k];
+std::cout << "after selection sort" << std::endl;
+    int QuickArray[length];
+    for(int k=0; k<length; k++){  //makes QuickArray a copy of Array
+        QuickArray[k]=Array[k];
     }
-    bucketSort(bucketArray, length);
+    PrintArray(length,QuickArray);
+    QuickSort(QuickArray, 0, length);
+std::cout << "after quick sort" << std::endl;
    // for(int k=0; k<length; k++){
-     //   cout<<"bucket: "<<bucketArray[k]<<endl;
+     //   cout<<"Quick: "<<QuickArray[k]<<endl;
     //}
-    
+std::cout << "Before Time" << std::endl;
      std::clock_t    start1;
 
      start1 = std::clock();
+std::cout << "Before Algorithm 1" << std::endl;
      if(isUnique1(Array,0,length)==true){
       cout<<"Algorithm 1 says it is unique."<< endl;
      }
+std::cout << "After Algorithm 1" << std::endl;
      std::cout << "Time: " << (std::clock() - start1) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
-     
+
      std::clock_t    start2;
 
      start2 = std::clock();
@@ -230,10 +196,11 @@ int main(){
       cout<<"Algorithm 2 says it is unique."<< endl;
      }
      std::cout << "Time: " << (std::clock() - start2) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
-     
+
      std::clock_t    start3;
 
      start3 = std::clock();
+     PrintArray(length,Array);
      if(isUnique3(Array, 0, length)==true){
       cout<<"Algorithm 3 says it is unique."<< endl;
      }
